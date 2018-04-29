7eb766596710194e995342f4a9eafa196f2da630
gangnobi <gangnobi@gmail.com>
2018-04-29 17:51:54 +0700

    new model

--- dataset_convertor.py
+++ dataset_convertor.py
@@ -9,6 +9,10 @@ import os
 from os.path import join
 import cv2
 
+from imutils import contours
+import imutils
+import numpy as np
+
 DATASET_PATH = join(os.getenv("HOME"), "Documents/NIST_Special_Database_19")
 DATASET_ORIGINAL_PATH = join(DATASET_PATH, "by_class")
 DATASET_RESULT_PATH = join(DATASET_PATH, "tensorflow_form")
@@ -43,4 +47,29 @@ for p in parentList:
         mypath = join(DATASET_ORIGINAL_PATH, p, "train_"+p)
         img = cv2.imread(join(mypath, name_file[p][i]))
         img = 255-img
-        cv2.imwrite(join(newpath, name_file[p][i][:-3]) + 'jpg', img)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        fit_img = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        fit_img = fit_img[0] if imutils.is_cv2() else fit_img[1]
+        
+        coordinate_x = []
+        coordinate_y = []
+        for c in fit_img:
+            (x, y, w, h) = cv2.boundingRect(c)
+            coordinate_x.append(x)
+            coordinate_x.append(x+w)
+            coordinate_y.append(y+h)
+            coordinate_y.append(y)
+        
+        if(max(coordinate_y)-min(coordinate_y) < max(coordinate_x)-min(coordinate_x)):
+            added = int(((max(coordinate_x)-min(coordinate_x))-(max(coordinate_y)-min(coordinate_y)))/2)
+            newimg = img[min(coordinate_y)-added:max(coordinate_y)+added,min(coordinate_x):max(coordinate_x)]
+        else:
+            added = int(((max(coordinate_y)-min(coordinate_y))-(max(coordinate_x)-min(coordinate_x)))/2)
+            newimg = img[min(coordinate_y):max(coordinate_y),min(coordinate_x)-added:max(coordinate_x)+added]
+
+        newimg = cv2.resize(newimg, (80, 80))
+        warp_bg = np.zeros((96, 96), dtype = img.dtype)
+        warp_bg[8:88,8:88] = newimg
+
+        cv2.imwrite(join(newpath, name_file[p][i][:-3]) + 'jpg', warp_bg)
